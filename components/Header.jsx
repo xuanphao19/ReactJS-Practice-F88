2f5ac68bc298236a4d8db70820c07c21299cffcc
Nguyễn Thanh Hòa <nguyenthanhhoa075@gmail.com>
2025-08-27 18:13:42 +0700

    Updates

---
+++ components/Header.jsx
@@ -0,0 +1,16 @@
+import ToggleTheme from "./toggleTheme";
+
+export default function Header() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1 className="slogan">
+        Conquer ReactJS with F8 🚀. <ToggleTheme />
+      </h1>
+      <p className="desc">
+        Dấu ấn khám phá và thực hành ReactJS cùng F8 -
+        <a href="https://fullstack.edu.vn/">fullstack.edu.vn</a> Học lập trình
+        để đi làm!
+      </p>
+    </div>
+  );
+}
